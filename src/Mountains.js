073e2bdc604a82d2b9a63e81cfb073bb2ce7081b
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-10-14 17:38:39 +0100

    moved mountain images to Mountains.js

--- src/Mountains.js
+++ src/Mountains.js
@@ -3,6 +3,91 @@ import React from "react";
 function Mountains() {
   return (
     <>
+    <defs>
+        <pattern
+          id="eriador-mountains-img"
+          x="0"
+          y="0"
+          width="1"
+          height="1"
+          viewBox="0 0 1024 576"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <image
+            width="1024"
+            height="576"
+            href={require("./images/eriador_mountains.png")}
+            alt="some mountains"
+          />
+        </pattern>
+        <pattern
+          className="mountains"
+          id="eriador-mountains-2-img"
+          x="0"
+          y="0"
+          width="1"
+          height="1"
+          viewBox="0 0 1024 576"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <image
+            width="1024"
+            height="576"
+            href={require("./images/eriador_mountains_2.png")}
+            alt="some mountains"
+          />
+        </pattern>
+        <pattern
+          className="mountains"
+          id="rhun-mountains"
+          x="1024"
+          y="576"
+          width="1"
+          height="1"
+          viewBox="0 0 1024 576"
+        >
+          <image
+            width="1024"
+            height="576"
+            href={require("./images/rhun-mountains.png")}
+            alt="some mountains"
+          />
+        </pattern>
+        <pattern
+          className="mountains"
+          id="mountains"
+          x="1024"
+          y="576"
+          width="1"
+          height="1"
+          viewBox="0 0 1024 576"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <image
+            width="1024"
+            height="576"
+            href={require("./images/mountains.png")}
+            alt="some mountains"
+          />
+        </pattern>
+        <pattern
+          className="mountains"
+          id="gondor-mountains"
+          x="0"
+          y="0"
+          width="1"
+          height="1"
+          viewBox="0 0 524 276"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <image
+            width="1024"
+            height="576"
+            href={require("./images/gondor-mountains.png")}
+            alt="some mountains"
+          />
+        </pattern>
+      </defs>
       <a className="mountains" id="eriador-mountains">
         <path
           fill="url(#eriador-mountains-img)"
