5ae082da9ea4e59a65d5f2c9681040843a5b417a
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-12-01 11:12:35 +0000

    created list of regions

---
+++ src/logic/Enums/Regions.ts
@@ -0,0 +1,19 @@
+import { Areas } from './Areas';
+import { Region } from '../Models/Region';
+import { Area } from '../Models/Area';
+import { AreaName } from './AreaNames';
+
+// TODO: update to use correct list of areas 
+const areas = [new Area(AreaName.Borderlands, false, false, [AreaName.Borderlands])];
+
+export const Regions = [
+    new Region('eriador', areas, 3),
+    new Region('arnor', areas, 7),
+    new Region('rhun', areas, 2),
+    new Region('mirkwood', areas, 4),
+    new Region('rohan', areas, 4),
+    new Region('rhovanion', areas, 4),
+    new Region('gondor', areas, 7),
+    new Region('mordor', areas, 2),
+    new Region('haradwaith', areas, 2),
+]
