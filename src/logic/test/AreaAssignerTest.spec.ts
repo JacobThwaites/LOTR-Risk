657e69dd7e55e5d4cfee6b5dafdc2f787ce1c017
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-12-01 00:02:11 +0000

    created test for AreaAssigner

--- src/logic/test/AreaAssignerTest.spec.ts
+++ src/logic/test/AreaAssignerTest.spec.ts
@@ -1,17 +1,28 @@
-import { Area } from '../Models/Area';
-import { AreaName} from '../Enums/AreaNames';
-import { TheShire } from '../Enums/AreaAdjacencies';
-import { assert } from 'chai';
-import 'mocha';
+import { Player } from "../Models/Player";
+import { Colour } from "../Enums/Colours";
+import { AreaAssigner } from "../Controllers/AreaAssigner";
+import { assert } from "chai";
+import "mocha";
 
-describe('AreaAssigner', () => {
-    let areas: Area;
-    beforeEach(function () {
-        areas = new Area(AreaName.TheShire, false, true, TheShire);
-    })
+describe("AreaAssigner", () => {
+  let assigner: AreaAssigner;
+  let player1: Player;
+  let player2: Player;
+  beforeEach(function() {
+    player1 = new Player("player 1", Colour.Green, true, 30);
+    player2 = new Player("player 2", Colour.Green, true, 30);
 
-    // it('should have a name', () => {
-    //     const result = area.getName();
-    //     assert.equal(result, 'The Shire');
-    // });
+    const players = [player1, player2];
+    assigner = new AreaAssigner(players);
+  });
+
+  it('should be able to assign an area to a player', () => {
+      const before = player1.getAreas().length;
+      assert.equal(before, 0)
+
+      assigner.assignAreas();
+
+      const after = player1.getAreas().length;
+      assert.isAbove(after, 0);
+  });
 });
