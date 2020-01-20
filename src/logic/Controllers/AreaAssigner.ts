efb5993ec9ec4dbc66ab5fe8748ecacf2c4d035b
Jake Thwaites <jakethwaites@hotmail.co.uk>
2020-01-20 21:50:52 +0000

    created method to shuffle areas

--- src/logic/Controllers/AreaAssigner.ts
+++ src/logic/Controllers/AreaAssigner.ts
@@ -1,4 +1,5 @@
 import { Player } from "../Models/Player";
+import { Area } from "../Models/Area";
 import { Areas } from "../Enums/Areas";
 
 export class AreaAssigner {
@@ -9,18 +10,38 @@ export class AreaAssigner {
 
   assignAreas() {
     const areas = Object.values(Areas);
-    
+    const shuffledAreas = this.shuffle(areas);
+
     let playerIndex = 0;
 
-    for (let i = 0; i < areas.length; i++) {
-      this.players[playerIndex].addArea(areas[i]);
-      areas[i].setPlayer(this.players[playerIndex]);
+    for (let i = 0; i < shuffledAreas.length; i++) {
+      this.players[playerIndex].addArea(shuffledAreas[i]);
+      shuffledAreas[i].setPlayer(this.players[playerIndex]);
       ++playerIndex;
     
       if (playerIndex >= this.players.length) {
         playerIndex = 0;
       }
     }
+  }
+
+  shuffle(areas: Array<Area>) {
+    let currentIndex = areas.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = areas[currentIndex];
+      areas[currentIndex] = areas[randomIndex];
+      areas[randomIndex] = temporaryValue;
+    }
+  
+    return areas;
   }
 
   getPlayers() {
