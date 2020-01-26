e513cc4edd8e1266681e9759512c75522a5a4ff4
Jake Thwaites <jakethwaites@hotmail.co.uk>
2020-01-26 16:29:38 +0000

    removed unused images

--- src/images/MountainImages.js
+++ src/images/MountainImages.js
@@ -4,51 +4,18 @@ function MountainImages() {
   return (
     <defs>
       <pattern
-        id="eriador-mountains-img"
-        x="0"
-        y="0"
-        width="1"
-        height="1"
-        viewBox="0 0 1024 576"
-        preserveAspectRatio="xMidYMid slice"
-      >
-        <image
-          width="1024"
-          height="576"
-          href={require("../images/eriador_mountains.png")}
-          alt="some mountains"
-        />
-      </pattern>
-      <pattern
-        className="mountains"
-        id="eriador-mountains-2-img"
-        x="0"
-        y="0"
-        width="2"
-        height="2"
-        viewBox="0 0 1024 576"
-        preserveAspectRatio="xMidYMid slice"
-      >
-        <image
-          width="1024"
-          height="576"
-          href={require("../images/mountains.png")}
-          alt="some mountains"
-        />
-      </pattern>
-      <pattern
         classNaxe="mountains"
         id="rhun-mountains"
         x="1024"
         y="576"
-        width="5"
-        height="5"
+        width="6"
+        height="6"
         viewBox="0 0 1024 576"
       >
         <image
           width="1024"
           height="576"
-          href={require("../images/m.png")}
+          href={require("../images/mountains2.png")}
           alt="some mountains"
         />
       </pattern>
@@ -82,24 +49,7 @@ function MountainImages() {
         <image
           width="1024"
           height="576"
-          href={require("../images/m.png")}
-          alt="some mountains"
-        />
-      </pattern>
-      <pattern
-        className="mountains"
-        id="gondor-mountains"
-        x="0"
-        y="0"
-        width="2"
-        height="2"
-        viewBox="0 0 524 276"
-        preserveAspectRatio="xMidYMid slice"
-      >
-        <image
-          width="1024"
-          height="576"
-          href={require("../images/m.png")}
+          href={require("../images/mountains2.png")}
           alt="some mountains"
         />
       </pattern>
