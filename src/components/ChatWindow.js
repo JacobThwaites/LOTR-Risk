3b38ad84b93886d6743963835597bf162c7a4dd5
Jake Thwaites <jakethwaites@hotmail.co.uk>
2020-04-04 16:28:48 +0100

    created basic chat window to add messages

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,46 +1,60 @@
 import React, { Component } from "react";
-import { Launcher } from "react-chat-window";
+import FormButton from "./common/FormButton";
 
 class ChatWindow extends Component {
-  constructor() {
-    super();
+  constructor({ props }) {
+    super(props);
     this.state = {
-      messageList: []
+      chatInput: '',
+      chatHandle: 'test',
+      messages: []
     };
+
+    this.onTextChange = this.onTextChange.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
   }
 
-  _onMessageWasSent(message) {
-    this.setState({
-      messageList: [...this.state.messageList, message]
-    });
+  onTextChange(event) {
+    this.setState({ chatInput: event.target.value });
   }
 
-  _sendMessage(text) {
-    if (text.length > 0) {
-      this.setState({
-        messageList: [
-          ...this.state.messageList,
-          {
-            author: "them",
-            type: "text",
-            data: { text }
-          }
-        ]
-      });
-    }
+  onSubmitMessage() {
+    const { chatInput, chatHandle, messages } = this.state;
+    const message = `${chatHandle}: ${chatInput}`
+    messages.push(message);
+    this.setState({ messages });
   }
 
   render() {
+    const { chatInput, chatHandle, messages } = this.state;
     return (
-      <div class='chat-window'>
-        <Launcher
-          agentProfile={{
-            teamName: "Risk",
-            imageUrl: "../../public/the-one-ring.jpeg"
-          }}
-          onMessageWasSent={this._onMessageWasSent.bind(this)}
-          messageList={this.state.messageList}
-          showEmoji
+      <div id="chat">
+        <h2>Chat</h2>
+        <div id="chat--window">
+          <div id="chat--output">
+            {messages.map((message) => {
+              return <p>{message}</p>
+            })}
+          </div>
+          <div id="chat--feedback"></div>
+        </div>
+        <input 
+          id="chat--handle" 
+          type="text" 
+          placeholder="Handle" 
+          value={chatHandle}
+        />
+        <input 
+          id="chat--message" 
+          type="text"
+          placeholder="Message" 
+          value={chatInput}
+          onChange={this.onTextChange}
+        />
+        <FormButton 
+          id="chat--send-button"
+          label="send"
+          onClick={this.onSubmitMessage}
         />
       </div>
     );
