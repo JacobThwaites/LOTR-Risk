44113d6115a51a7ce202a84671ef244ec3861984
Jake Thwaites <jakethwaites@hotmail.co.uk>
2020-04-17 22:51:27 +0100

    center nameSelector in page

--- src/components/NameSelector.js
+++ src/components/NameSelector.js
@@ -4,10 +4,12 @@ import FormButton from "./common/FormButton";
 function NameSelector({ onChangeName, playerName, onSubmit }) {
   return (
     <div className='name-selector'>
-      <input onChange={onChangeName} value={playerName}/>
+      <label className='name-selector--label'>Choose Name: </label>
+      <input className='name-selector--input' onChange={onChangeName} value={playerName}/>
       <FormButton 
         onClick={onSubmit}
         label='Submit'
+        id='name-selector--button'
       />
     </div>
   );
