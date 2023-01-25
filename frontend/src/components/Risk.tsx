877ffe5a2b2eddb2f4468ea781b83dcb1f86b512
Jacob Thwaites <jakethwaites@gmail.com>
2023-01-25 09:43:58 +0100

    convert Risk component from class to functional

--- frontend/src/components/Risk.tsx
+++ frontend/src/components/Risk.tsx
@@ -1,115 +1,90 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import GameSetup from "./GameSetup";
 import { Redirect } from "react-router";
-import { generateURL } from "../logic/Controllers/URLGenerator";
+import { createGame } from "../logic/Controllers/URLGenerator";
 
-type Props = {
-};
+export default function Risk() {
+    const [numberOfPlayers, setNumberOfPlayers] = useState(0);
+    const [shouldDisplayChooseGameType, setShouldDisplayChooseGameType] = useState(true);
+    const [shouldDisplayNumberOfPlayersSelector, setShouldDisplayNumberOfPlayersSelector] = useState(false);
+    const [shouldDisplayNameSelector, setShouldDisplayNameSelector] = useState(false);
+    const [shouldDisplayGameSetup, setShouldDisplayGameSetup] = useState(true);
+    const [playerName, setPlayerName] = useState('');
+    const [gameType, setGameType] = useState('online');
 
-type State = {
-  numberOfPlayers: number,
-  shouldDisplayChooseGameType: boolean
-  shouldDisplayNumberOfPlayersSelector: boolean,
-  shouldDisplayNameSelector: boolean,
-  shouldDisplayGameSetup: boolean,
-  playerName: string,
-  gameType: string
-};
-
-export default class Risk extends Component<Props, State> {
-  constructor( props: Props ) {
-    super(props);
-    this.state = {
-      numberOfPlayers: 0,
-      shouldDisplayChooseGameType: true,
-      shouldDisplayNumberOfPlayersSelector: false,
-      shouldDisplayNameSelector: false,
-      shouldDisplayGameSetup: true,
-      playerName: "",
-      gameType: "online",
-    };
-
-    this.onNumberSelect = this.onNumberSelect.bind(this);
-    this.onChangeName = this.onChangeName.bind(this);
-    this.onSubmitNumberOfPlayers = this.onSubmitNumberOfPlayers.bind(this);
-    this.onSubmitName = this.onSubmitName.bind(this);
-    this.onSubmitGameType = this.onSubmitGameType.bind(this);
-  }
-
-  onNumberSelect(number: number, b: any, a: {name: string}) {
-    const { name } = a;
-    const newState = { [name]: number } as unknown as Pick<State, keyof State>;
-    this.setState(newState);
-  }
-
-  onChangeName(event: any) {
-    const playerName = event.target.value;
-    this.setState({ playerName });
-  }
-
-  onSubmitNumberOfPlayers() {
-    this.setState({
-      shouldDisplayNumberOfPlayersSelector: false,
-      shouldDisplayNameSelector: true,
-    });
-  }
-
-  onSubmitName() {
-    this.setState({ shouldDisplayGameSetup: false });
-  }
-
-  onSubmitGameType(gameType: string) {
-    this.setState({
-      gameType,
-      shouldDisplayChooseGameType: false,
-      shouldDisplayNumberOfPlayersSelector: true,
-    });
-  }
-  
-  renderGameSetup() {
-    const {
-      numberOfPlayers,
-      playerName,
-      shouldDisplayNumberOfPlayersSelector,
-      shouldDisplayChooseGameType,
-    } = this.state;
-    return (
-      <GameSetup
-        numberOfPlayers={numberOfPlayers}
-        playerName={playerName}
-        shouldDisplayChooseGameType={shouldDisplayChooseGameType}
-        shouldDisplayNumberOfPlayersSelector={
-          shouldDisplayNumberOfPlayersSelector
-        }
-        onChangeNumberOfPlayers={this.onNumberSelect}
-        onSubmitNumberOfPlayers={this.onSubmitNumberOfPlayers}
-        onChangeName={this.onChangeName}
-        onSubmitPlayerName={this.onSubmitName}
-        onSubmitGameType={this.onSubmitGameType}
-      />
-    );
-  }
-
-  renderGame() {
-    const { numberOfPlayers, playerName } = this.state;
-    const url = generateURL();
-    return (
-      <Redirect
-        to={{
-          pathname: url,
-          state: { numberOfPlayers, playerName },
-        }}
-      />
-    );
-  }
-
-  render() {
-    const { shouldDisplayGameSetup } = this.state;
-
-    if (shouldDisplayGameSetup) {
-      return this.renderGameSetup();
+    function onNumberSelect(number: number, b: any, a: { name: string }) {
+        setNumberOfPlayers(number);
     }
 
-    return this.renderGame();
-  }
+    function onChangeName(event: any) {
+        const playerName = event.target.value;
+        setPlayerName(playerName);
+    }
+
+    function onSubmitNumberOfPlayers() {
+        setShouldDisplayNumberOfPlayersSelector(false);
+        setShouldDisplayNameSelector(true);
+    }
+
+    async function onSubmitName() {
+        setShouldDisplayGameSetup(false);
+    }
+
+    function onSubmitGameType(gameType: string) {
+        setGameType(gameType);
+        setShouldDisplayChooseGameType(false);
+        setShouldDisplayNumberOfPlayersSelector(true);
+    }
+
+    if (shouldDisplayGameSetup) {
+        return (
+            <GameSetup
+                numberOfPlayers={numberOfPlayers}
+                playerName={playerName}
+                shouldDisplayChooseGameType={shouldDisplayChooseGameType}
+                shouldDisplayNumberOfPlayersSelector={
+                    shouldDisplayNumberOfPlayersSelector
+                }
+                onChangeNumberOfPlayers={onNumberSelect}
+                onSubmitNumberOfPlayers={onSubmitNumberOfPlayers}
+                onChangeName={onChangeName}
+                onSubmitPlayerName={onSubmitName}
+                onSubmitGameType={onSubmitGameType}
+            />
+        );
+    }
+
+    return <GameRedirect numberOfPlayers={numberOfPlayers} playerName={playerName} />
+}
+
+function GameRedirect(props: { numberOfPlayers: number, playerName: string }) {
+    const [uuid, setUuid] = useState('');
+
+    const getData = async () => {
+        try {
+            const game = await createGame(props.numberOfPlayers);
+            const json = await game!.json()
+            const { uuid } = json.data;
+            setUuid(uuid);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    if (!uuid) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <Redirect
+            to={{
+                pathname: uuid,
+                state: { numberOfPlayers: props.numberOfPlayers, playerName: props.playerName },
+            }}
+        />
+    );
 }
