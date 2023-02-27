ba179e1cf5aa3a6bf0e751db4d8d8fc00672c26d
Jacob Thwaites <jakethwaites@gmail.com>
2023-02-27 15:00:30 +0100

    update image and text for WaitingForPlayers page

--- frontend/src/components/WaitingForPlayers.tsx
+++ frontend/src/components/WaitingForPlayers.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { ReactComponent as RingSVG } from '../assets/ring-loading.svg';
 
 export default function WaitingForPlayers(props: { playersLeftToJoin: number }) {
     return (
-        <>
+        <div id='waiting'>
+            <RingSVG id='waiting-image'/>
             <h1>Waiting for {props.playersLeftToJoin} more players to join...</h1>
-        </>
+            <h1>Share this url to allow others to join: {window.location.href}</h1>
+        </div>
     )
 }
