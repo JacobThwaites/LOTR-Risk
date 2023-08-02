2dc64a458d01930748c8501d26dba767bf787084
Jacob Thwaites <jakethwaites@gmail.com>
2023-08-02 11:13:49 +0100

    test GameDisplay not found screen

--- frontend/src/components/GameDisplay.spec.tsx
+++ frontend/src/components/GameDisplay.spec.tsx
@@ -8,7 +8,27 @@ import { addUserIDToGame } from '../gameLogic/Controllers/requests';
 jest.mock('../gameLogic/Controllers/requests');
 jest.mock('react-router');
 
-it('renders Waiting for players page after game load', async () => {
+it('renders game not found page if invalid gameID is provided', async () => {
+    const mockGameID = '1234';
+    (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
+    (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
+    const mockResponse = {
+        ok: false
+    };
+    (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
+    
+    let container: any;
+    await act(() => {
+        container = render(<GameDisplay />).container;
+    })
+    
+    expect(addUserIDToGame).toHaveBeenCalledWith(mockGameID);
+    const notFoundScreen = await screen.findByTestId('not-found');
+    expect(notFoundScreen).toBeTruthy();
+    expect(container).toMatchSnapshot();
+});
+
+it('renders waiting for players page after online game load', async () => {
     const mockGameID = '1234';
     (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
     (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function NotFound() {
     return ( 
-        <div className='not-found'>
+        <div className='not-found' data-testid='not-found'>
             <h1>Game Not Found</h1> 
             <p>No game could be found with the given URL</p>
             <a href={`${process.env.PUBLIC_URL}/`}>Click here to return home</a>
