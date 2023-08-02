267aee68ffd854d7407c928e0f3adadc3fd77ba8
Jacob Thwaites <jakethwaites@gmail.com>
2023-08-02 12:26:43 +0100

    wrap tests in describe with beforeEach

--- frontend/src/components/GameDisplay.spec.tsx
+++ frontend/src/components/GameDisplay.spec.tsx
@@ -4,7 +4,6 @@ import { render, screen, fireEvent, act } from '@testing-library/react'
 import GameDisplay from './GameDisplay';
 import { useParams, useLocation } from 'react-router';
 import { addUserIDToGame } from '../gameLogic/Controllers/requests';
-import makeWebSocketHandler from '../utils/makeWebSocketHandler';
 
 jest.mock('react-router');
 jest.mock('../gameLogic/Controllers/requests');
@@ -28,70 +27,71 @@ jest.mock('../utils/makeWebSocketHandler', () => {
     }
 });
 
-it('renders game not found page if invalid gameID is provided', async () => {
-    const mockGameID = '1234';
-    (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
-    (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
-    const mockResponse = {
-        ok: false
-    };
-    (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
-    
-    let container: any;
-    await act(() => {
-        container = render(<GameDisplay />).container;
-    })
+describe('GameDisplay', function () {
+    let mockGameID: string;
 
-    const notFoundScreen = await screen.findByTestId('not-found');
-    expect(notFoundScreen).toBeTruthy();
-    expect(container).toMatchSnapshot();
-});
-
-it('renders waiting for players page after online game load', async () => {
-    const mockGameID = '1234';
-    (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
-    (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
-    const mockPlayer = {colour: 'Black', reinforcements: 2, areas:[]};
-    const mockResponse = {
-        ok: true, json: async () => ({ data: { uuid: mockGameID, players: [mockPlayer], maxTurns: 1 } }),
-    };
-    (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
-    
-    let container: any;
-    await act(() => {
-        container = render(<GameDisplay />).container;
+    beforeEach(() => {
+        mockGameID = '1234';
+        (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
+        (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
     })
     
-    expect(addUserIDToGame).toHaveBeenCalledWith(mockGameID);
-    const waitingForPlayersScreen = await screen.findByTestId('waiting-for-players-screen');
-    expect(waitingForPlayersScreen).toBeTruthy();
-    expect(container).toMatchSnapshot();
-});
-
-it('renders GameDisplay after all players are joined', async () => {
-    const mockGameID = '1234';
-    (useParams as jest.Mock).mockReturnValue({gameID: mockGameID});
-    (useLocation as jest.Mock).mockReturnValue({location: {state: {gameType: 'local'}}});
-
-    const mockPlayer = {colour: 'Black', reinforcements: 2, areas:[]};
-    const mockResponse = {
-        ok: true, json: async () => ({ data: { uuid: mockGameID, players: [mockPlayer], maxTurns: 1 } }),
-    };
-    (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
+    it('renders game not found page if invalid gameID is provided', async () => {
+        const mockResponse = {
+            ok: false
+        };
+        (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
+        
+        let container: any;
+        await act(() => {
+            container = render(<GameDisplay />).container;
+        })
     
-    let rerender: any;
-    await act(() => {
-        const renderResponse = render(<GameDisplay />);
-        rerender = renderResponse.rerender;
-    })
+        const notFoundScreen = await screen.findByTestId('not-found');
+        expect(notFoundScreen).toBeTruthy();
+        expect(container).toMatchSnapshot();
+    });
     
-    expect(addUserIDToGame).toHaveBeenCalledWith(mockGameID);
-    rerender(<GameDisplay />);
-    const gameDisplay = await screen.findByTestId('game-display');
-    expect(gameDisplay).toBeTruthy();
-});
+    it('renders waiting for players page after online game load', async () => {
+        const mockPlayer = {colour: 'Black', reinforcements: 2, areas:[]};
+        const mockResponse = {
+            ok: true, json: async () => ({ data: { uuid: mockGameID, players: [mockPlayer], maxTurns: 1 } }),
+        };
+        (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
+        
+        let container: any;
+        await act(() => {
+            container = render(<GameDisplay />).container;
+        })
+        
+        expect(addUserIDToGame).toHaveBeenCalledWith(mockGameID);
+        const waitingForPlayersScreen = await screen.findByTestId('waiting-for-players-screen');
+        expect(waitingForPlayersScreen).toBeTruthy();
+        expect(container).toMatchSnapshot();
+    });
+    
+    it('renders GameDisplay after all players are joined', async () => {
+        const mockPlayer = {colour: 'Black', reinforcements: 2, areas:[]};
+        const mockResponse = {
+            ok: true, json: async () => ({ data: { uuid: mockGameID, players: [mockPlayer], maxTurns: 1 } }),
+        };
+        (addUserIDToGame as jest.Mock).mockResolvedValueOnce(mockResponse);
+        
+        let rerender: any;
+        await act(() => {
+            const renderResponse = render(<GameDisplay />);
+            rerender = renderResponse.rerender;
+        })
+        
+        expect(addUserIDToGame).toHaveBeenCalledWith(mockGameID);
+        rerender(<GameDisplay />);
+        const gameDisplay = await screen.findByTestId('game-display');
+        expect(gameDisplay).toBeTruthy();
+    });
+    
+    it('renders game details from fetch response', async () => {
+        // TODO: implement
+        expect(true).toBeTruthy();
+    });
+})
 
-it('renders game details from fetch response', async () => {
-    // TODO: implement
-    expect(true).toBeTruthy();
-});
