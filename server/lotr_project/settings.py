5154047bf65b48a160aa812a93057cd2380e1de1
Jacob Thwaites <jakethwaites@gmail.com>
2024-08-22 22:33:16 +0200

    upgrade server requirements to use django 5.1

--- server/lotr_project/settings.py
+++ server/lotr_project/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'lotr_project.urls'
